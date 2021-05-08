dd50909158b81ec8e5fa645bee2833f5a1972129
martoriz <dimon787@ukr.net>
2021-05-08 23:07:30 +0300

    --- Auto-Git Commit ---

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,4 +33,17 @@ const Content = styled.div`
 
 `;
 
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;   
+    background-image: url("/images/login-background.jpg");
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1; 
+    `;
+
 export default Login; 
